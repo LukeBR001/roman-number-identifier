0b79ec13bbd47aead133b9aa4130497734b5e42d
Lucas Eduardo <lucas.eduardo@m4u.com.br>
2022-05-08 16:28:45 -0300

    feature/implement all hard logic to separate and validate roman numbers

--- pkg/service.go
+++ pkg/service.go
@@ -1,7 +1,205 @@
 package pkg
 
-import "identifier/pkg/model"
+import (
+	"errors"
+	"identifier/pkg/model"
+	"strings"
+)
 
-func CountRomanNumber(textPayload model.TextPayload) (model.TextPayload, error) {
-	return textPayload, nil
+var validCombinations = []string{
+	"CM",
+	"CD",
+	"XC",
+	"XL",
+	"IX",
+	"IV",
+}
+
+var RomanNumbers = model.DefinedReferenceSymbols{
+	Combinations: map[string]int{
+		"I": 1,
+		"V": 5, //não pode repetir
+		"X": 10,
+		"L": 50, //não pode repetir
+		"C": 100,
+		"D": 500, //não pode repetir
+		"M": 1000,
+	},
+}
+
+type Service interface {
+	IdentifyCombinations(textPayload model.TextPayload) ([]string, error)
+	IdentifyBiggerNumber(romanList []string) int
+}
+
+// 1° - Identifico as combinações romanas em uma cadeira de caracteres
+// 2° - Coloco as respectivas combinações em um novo map.
+// 3° - Identifico a combinação com maior valor numérico.
+
+func IdentifyCombinations(textPayload model.TextPayload) ([]string, error) {
+
+	identifiedCombinations := make([]string, 0)
+	var str string
+
+	for _, v := range textPayload.Text {
+		vs := strings.ToUpper(string(v))
+		if _, ok := RomanNumbers.Combinations[vs]; ok {
+			str += vs
+		} else {
+			if str != "" {
+				identifiedCombinations = append(identifiedCombinations, str)
+			}
+			str = ""
+		}
+	}
+
+	if str != "" {
+		identifiedCombinations = append(identifiedCombinations, str)
+	}
+
+	var validRomanNumbers []string
+	for _, v := range identifiedCombinations {
+		if isValidRomanSequence(v) {
+			validRomanNumbers = append(validRomanNumbers, v)
+		}
+	}
+
+	if len(validRomanNumbers) == 0 {
+		return []string{}, errors.New("not found valid roman combinations")
+	}
+
+	return validRomanNumbers, nil
+}
+
+//func symbolDivisor(symbols []string) []string {
+//	var separatedSymbols []string
+//	addSymbol := symbols[0]
+//
+//	if len(symbols) == 1 {
+//		separatedSymbols = append(separatedSymbols, addSymbol)
+//		return separatedSymbols
+//	}
+//
+//	for i := 1; i < len(symbols); i++ {
+//		lastSymbol := addSymbol[len(addSymbol)-1:]
+//
+//		if symbols[i] == lastSymbol {
+//			if isRepeatable(symbols[i]) {
+//				if len(addSymbol) >= 3 {
+//					index := addSymbol[len(addSymbol)-3 : len(addSymbol)-2]
+//					if symbols[i] == index {
+//						separatedSymbols = append(separatedSymbols, addSymbol)
+//						addSymbol = symbols[i]
+//					} else {
+//						addSymbol += symbols[i]
+//					}
+//				} else {
+//					addSymbol += symbols[i]
+//				}
+//			} else {
+//				separatedSymbols = append(separatedSymbols, addSymbol)
+//				addSymbol = symbols[i]
+//			}
+//		} else {
+//			addSymbol += symbols[i]
+//		}
+//	}
+//	separatedSymbols = append(separatedSymbols, addSymbol)
+//
+//	return separatedSymbols
+//}
+
+func isRepeatable(symbol string) bool {
+	if symbol == "I" || symbol == "X" || symbol == "C" || symbol == "M" {
+		return true
+	}
+	return false
+}
+
+func IdentifyBiggerNumber(romanList []string) int {
+	var biggerGlobalNumber int
+
+	for _, v := range romanList {
+		var sumLocalNumbers int
+
+		if len(v) > 1 {
+			leftNumber := RomanNumbers.Combinations[string(v[0])]
+			sumLocalNumbers = leftNumber
+
+			for j := 1; j < len(v); j++ {
+
+				actualString := string(v[j])
+				actualNumber := RomanNumbers.Combinations[actualString]
+
+				if actualNumber <= leftNumber {
+					sumLocalNumbers += actualNumber
+
+				} else {
+					sumLocalNumbers = sumLocalNumbers + actualNumber - (2 * leftNumber)
+				}
+				leftNumber = RomanNumbers.Combinations[string(v[j])]
+			}
+			if sumLocalNumbers > biggerGlobalNumber {
+				biggerGlobalNumber = sumLocalNumbers
+			}
+
+		} else {
+			sumLocalNumbers = RomanNumbers.Combinations[string(v[0])]
+			if sumLocalNumbers > biggerGlobalNumber {
+				biggerGlobalNumber = RomanNumbers.Combinations[string(v[0])]
+			}
+		}
+	}
+	return biggerGlobalNumber
+}
+
+func isValidRomanSequence(s string) bool {
+	if len(s) == 1 {
+		return true
+	}
+
+	var check bool
+	if len(s) > 1 {
+		SymbolValue := RomanNumbers.Combinations
+		leftSymbol := string(s[0])
+
+		for i := 1; i < len(s); i++ {
+			check = false
+			str := s[:i]
+			actualSymbol := string(s[i])
+
+			if SymbolValue[leftSymbol] > SymbolValue[actualSymbol] {
+				if strings.Contains(str, actualSymbol) {
+					return false
+				}
+
+				check = true
+			} else if SymbolValue[leftSymbol] == SymbolValue[actualSymbol] { // I == I
+				count := strings.Count(str, actualSymbol)
+
+				if isRepeatable(actualSymbol) {
+					if count < 3 {
+						check = true
+					}
+				}
+			} else {
+				if strings.Contains(str, actualSymbol) {
+					return false
+				}
+
+				combSymbol := leftSymbol + actualSymbol
+				for _, v := range validCombinations {
+					if combSymbol == v {
+						check = true
+					}
+				}
+			}
+			if !check {
+				return check
+			}
+			leftSymbol = actualSymbol
+
+		}
+	}
+	return check
 }
