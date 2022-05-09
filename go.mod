e6d438182be9d7c133042fa6fdeffab0bbe1f439
Lucas Eduardo <lucas.eduardo@m4u.com.br>
2022-05-09 12:22:14 -0300

    feature/add test cases to service function

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module identifier
 
 go 1.17
 
-require github.com/go-chi/chi/v5 v5.0.7 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.0.7 // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
+)
---
+++ pkg/service_test.go
@@ -0,0 +1,124 @@
+package pkg
+
+import (
+	"github.com/google/go-cmp/cmp"
+	"identifier/pkg/model"
+	"testing"
+)
+
+func TestIdentifyCombinations(t *testing.T) {
+	type args struct {
+		textPayload model.TextPayload
+	}
+
+	validComb := model.TextPayload{Text: "XIVQWECQWEMMDCCXXIVQWELXQWELXXXQWEXLQWEMmDcCcxXIv"}
+	//"XIV C MMDCCXXIV LX LXXX XL MmDcCcxXIv"
+
+	justInvalidComb := model.TextPayload{Text: "XIVIQWEVCQWEMMDCCDXXIVQWELXLQWELXXXXQWEXLLQWEMmDcCcxXIvX"}
+	randomComb := model.TextPayload{Text: "XIVQWEIIIIQWEMMDCCXXIVQWELLXQWELXXXQWEIXLQWEMmDcCcxXIvQWEICQWEXXL"}
+	//"XIV MMDCCXXIV LXXX MMDCCCXXIV"
+
+	tests := []struct {
+		name    string
+		args    args
+		want    []string
+		wantErr bool
+	}{
+		{
+			name: "should return all valid combinations when successful",
+			args: args{
+				textPayload: validComb,
+			},
+			want:    []string{"XIV", "C", "MMDCCXXIV", "LX", "LXXX", "XL", "MMDCCCXXIV"},
+			wantErr: false,
+		},
+		{
+			name: "should return error when not found any valid roman combinations",
+			args: args{
+				textPayload: justInvalidComb,
+			},
+			want:    []string{},
+			wantErr: true,
+		},
+		{
+			name: "should return just valid roman combinations",
+			args: args{
+				textPayload: randomComb,
+			},
+			want:    []string{"XIV", "MMDCCXXIV", "LXXX", "MMDCCCXXIV"},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := IdentifyCombinations(tt.args.textPayload)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("IdentifyCombinations() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Errorf("Got error, Diff = %s", diff)
+			}
+		})
+	}
+}
+
+func Test_isValidRomanSequence(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isValidRomanSequence(tt.args.s); got != tt.want {
+				t.Errorf("isValidRomanSequence() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestIdentifyBiggerNumber(t *testing.T) {
+	type args struct {
+		romanList []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IdentifyBiggerNumber(tt.args.romanList); got != tt.want {
+				t.Errorf("IdentifyBiggerNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_isRepeatable(t *testing.T) {
+	type args struct {
+		symbol string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isRepeatable(tt.args.symbol); got != tt.want {
+				t.Errorf("isRepeatable() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
